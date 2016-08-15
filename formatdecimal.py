c0ad2204f3910d30ec900d35153c8d5c1441e100
alexhendra <alexhendra_panjaitan@yahoo.com>
2016-08-15 11:00:34 +0700

    Awal memulai belajar

---
+++ formatdecimal.py
@@ -0,0 +1,4 @@
+import locale
+locale.setlocale(locale.LC_ALL,"")
+
+print locale.format("%.2f",1234.567,1)
---
+++ stringnya.py
@@ -0,0 +1,28 @@
+# untuk menulis kutip didalam ""
+# kalimat = 'doesn\'t'
+# print kalimat
+
+# string multi baris
+# kalimatpanjang = """Apa kabar semua ?
+# Kalian baik2 saja kan"""
+
+# print kalimatpanjang
+
+# Concat string
+# a = "Bahasa"
+# b = "Python"
+# c = a + ' ' + b
+# print c
+
+# Perkalian string
+# a = '-' * 10
+# print a
+
+# String menjadi bilangan
+# a = int("8")
+# b = float("8.9")
+# print a
+# print b
+
+# Mengetahui panjan string
+print len("Alex Hendra Panjaitan")
